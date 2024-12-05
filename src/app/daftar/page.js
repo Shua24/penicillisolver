a9e6450d49b404551a3e38fd41ad17be7b274c99
Muhammad Ghiyats Fatiha <daffaadriantoe23@gmail.com>
2024-12-05 22:01:49 +0700

    Memperbaiki CSS daftar

--- src/app/daftar/page.js
+++ src/app/daftar/page.js
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "./daftar.module.css"; // Assuming you renamed the file as needed
 import { auth, db } from "./firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -78,144 +77,144 @@ const Daftar = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
-        <Image
-          src="/dokter1.png"
-          alt="Gambar Dokter"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-      <div className={styles.form}>
-        <div className={styles.logo}>
-          <Link href="/Landing/landing">
-            <Image src="/logo.png" alt="logo" width={100} height={50} />
-          </Link>
+    <div className={styles.dasar}>
+      <div className={styles.container}>
+        <div className={styles.background}>
+          <img
+            src="/dokter1.png"
+            alt="Gambar Dokter"
+          />
         </div>
-        <div className={styles.formContainer}>
-          <div className={styles.selamat}>
-            <h1>Selamat Datang</h1>
-            <h3>Silakan Daftar</h3>
+        <div className={styles.form}>
+          <div className={styles.logo}>
+            <Link href="/landing">
+              <img src="/logo.png" alt="logo" width={100} height={60} />
+            </Link>
           </div>
-          <form onSubmit={handleRegister}>
-            {error && <p className={styles.error}>{error}</p>}
-            <label htmlFor="nama" className={styles.label}>Nama Lengkap</label>
-            <input
-              className={styles.input}
-              type="text"
-              id="nama"
-              name="nama"
-              value={formData.nama}
-              onChange={handleInputChange}
-              required
-              autoComplete="off"
-            />
-            <label htmlFor="email" className={styles.label}>Email</label>
-            <input
-              className={styles.input}
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              required
-              autoComplete="off"
-            />
-            <label htmlFor="sip" className={styles.label}>Nomor SIP</label>
-            <input
-              className={styles.input}
-              type="text"
-              id="sip"
-              name="sip"
-              value={formData.sip}
-              onChange={handleInputChange}
-              autoComplete="off"
-            />
-            <label htmlFor="password" className={styles.label}>Password</label>
-            <div className={styles.passwordInput}>
+          <div className={styles.formContainer}>
+            <div className={styles.selamat}>
+              <h1>Selamat Datang</h1>
+              <h3>Silakan Daftar</h3>
+            </div>
+            <form onSubmit={handleRegister}>
+              {error && <p className={styles.error}>{error}</p>}
+              <label htmlFor="nama" className={styles.label}>Nama Lengkap</label>
               <input
                 className={styles.input}
-                type={isPasswordVisible ? "text" : "password"}
-                id="password"
-                name="password"
-                value={formData.password}
+                type="text"
+                id="nama"
+                name="nama"
+                value={formData.nama}
                 onChange={handleInputChange}
                 required
+                autoComplete="off"
               />
-              <button
-                className={styles.passwordButton}
-                type="button"
-                onClick={() => setPasswordVisible(!isPasswordVisible)}
-              >
-                {isPasswordVisible ? "Hide" : "Show"}
-              </button>
-            </div>
-            <label htmlFor="confirmPassword" className={styles.label}>Konfirmasi Password</label>
-            <div className={styles.passwordInput}>
+              <label htmlFor="email" className={styles.label}>Email</label>
               <input
                 className={styles.input}
-                type={isConfirmPasswordVisible ? "text" : "password"}
-                id="confirmPassword"
-                name="confirmPassword"
-                value={formData.confirmPassword}
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
                 onChange={handleInputChange}
                 required
+                autoComplete="off"
               />
-              <button
-                className={styles.passwordButton}
-                type="button"
-                onClick={() => setConfirmPasswordVisible(!isConfirmPasswordVisible)}
-              >
-                {isConfirmPasswordVisible ? "Hide" : "Show"}
-              </button>
-            </div>
-            <div className={styles.pilih}>
-              <label>Daftar sebagai:</label>
-            </div>
-            <div className={styles.roleSelection}>
-              {["Mikrobiologi", "Dokter lain", "PPI", "PPRA", "Penanggung Jawab Lab"].map((role) => (
+              <label htmlFor="sip" className={styles.label}>Nomor SIP</label>
+              <input
+                className={styles.input}
+                type="text"
+                id="sip"
+                name="sip"
+                value={formData.sip}
+                onChange={handleInputChange}
+                autoComplete="off"
+              />
+              <label htmlFor="password" className={styles.label}>Password</label>
+              <div className={styles.passwordInput}>
+                <input
+                  className={styles.input}
+                  type={isPasswordVisible ? "text" : "password"}
+                  id="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  required
+                />
                 <button
-                  key={role}
+                  className={styles.passwordButton}
                   type="button"
-                  className={formData.role === role ? styles.selected : ""}
-                  onClick={() => selectRole(role)}
+                  onClick={() => setPasswordVisible(!isPasswordVisible)}
                 >
-                  {role}
+                  {isPasswordVisible ? "Hide" : "Show"}
                 </button>
-              ))}
+              </div>
+              <label htmlFor="confirmPassword" className={styles.label}>Konfirmasi Password</label>
+              <div className={styles.passwordInput}>
+                <input
+                  className={styles.input}
+                  type={isConfirmPasswordVisible ? "text" : "password"}
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleInputChange}
+                  required
+                />
+                <button
+                  className={styles.passwordButton}
+                  type="button"
+                  onClick={() => setConfirmPasswordVisible(!isConfirmPasswordVisible)}
+                >
+                  {isConfirmPasswordVisible ? "Hide" : "Show"}
+                </button>
+              </div>
+              <div className={styles.pilih}>
+                <label>Daftar sebagai:</label>
+              </div>
+              <div className={styles.roleSelection}>
+                {["Mikrobiologi", "Dokter lain", "PPI", "PPRA", "Penanggung Jawab Lab"].map((role) => (
+                  <button
+                    key={role}
+                    type="button"
+                    className={formData.role === role ? styles.selected : ""}
+                    onClick={() => selectRole(role)}
+                  >
+                    {role}
+                  </button>
+                ))}
+              </div>
+              <button type="submit" className={styles.daftar}>
+                Daftar
+              </button>
+            </form>
+            <div className={styles.login}>
+              Sudah Punya Akun? <Link href="/login">Login</Link>
             </div>
-            <button type="submit" className={styles.daftar}>
-              Daftar
-            </button>
-          </form>
-          <div className={styles.login}>
-            Sudah Punya Akun? <Link href="/login">Login</Link>
-          </div>
-          <div className={styles.lanjutkan}>
-            <p>Atau lanjutkan dengan :</p>
-            <Link href="http://www.google.com" target="_blank">
-              <Image
-              src="/google.png"
-              alt="Google"
-              width={75}
-              height={75} />
-            </Link>
-            <Link href="http://www.facebook.com" target="_blank">
-              <Image
-              src="/fb.png"
-              alt="Facebook"
-              width={75}
-              height={75} />
-            </Link>
-            <Link href="https://x.com/?lang=en" target="_blank">
-              <Image 
-              src="/x.png"
-              alt="X"
-              className={styles.twitterLogo}
-              width={75}
-              height={75} />
-            </Link>
+            <div className={styles.lanjutkan}>
+              <p>Atau lanjutkan dengan :</p>
+              <Link href="http://www.google.com" target="_blank">
+                <img
+                src="/google.png"
+                alt="Google"
+                width={60}
+                height={60} />
+              </Link>
+              <Link href="http://www.facebook.com" target="_blank">
+                <img
+                src="/fb.png"
+                alt="Facebook"
+                width={60}
+                height={60} />
+              </Link>
+              <Link href="https://x.com/?lang=en" target="_blank">
+                <img 
+                src="/x.png"
+                alt="X"
+                className={styles.twitterLogo}
+                width={60}
+                height={60} />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
