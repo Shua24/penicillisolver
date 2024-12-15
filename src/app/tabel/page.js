4a2c3f642d03a5957c99907849360c3a3c6123d6
shua24 <jo.daniel.simanjuntak@gmail.com>
2024-12-15 12:45:58 +0700

    Tambah upload ke Firebase

--- src/app/tabel/page.js
+++ src/app/tabel/page.js
@@ -6,6 +6,8 @@ import Sidebar from '../sidebar/page';
 
 const Tabel = () => {
   const [jsonData, setJsonData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [postResponse, setPostResponse] = useState(null);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_TABLE_API_URL}/api/excel-data`) // URL ada di .env
@@ -13,13 +15,41 @@ const Tabel = () => {
       .then((data) => setJsonData(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
+
+  const handlePostRequest = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_TABLE_QUERY_URL}/upload-to-firebase`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+
+      const result = await response.json();
+      setPostResponse(result.message);
+      alert('Pola kuman ter-upload ke database!');
+      setTimeout(() => setPostResponse(null), 2000);
+    } catch (error) {
+      console.error('Error posting data:', error);
+      alert('Failed to upload data.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
    const rows = Object.values(jsonData);
    const headers = rows.length > 0 ? Object.values(rows[0]) : [];
  
    const dataRows = rows.slice(1);
  
    return (
-    <div className={styles.pageContainer}><Sidebar/>
+    <div className={styles.pageContainer}>
+      <Sidebar/>
      <div className={styles.tableWrapper}>
        <h1 className={styles.title}>Pola Kuman Tahun Ini</h1>
        <div className={styles.tableContainer}>
@@ -41,7 +71,19 @@ const Tabel = () => {
            ))}
          </tbody>
        </table>
-       </div>
+       </div> <br/>
+       <button
+          className={`${styles.button} ${isLoading ? styles.uploading : ''}`}
+          onClick={handlePostRequest}
+          disabled={isLoading}
+        >
+          {isLoading ? 'Mengupload' : 'Upload ke database'}
+        </button>
+        {postResponse && (
+          <div className={styles.text}>
+            <p>{postResponse}</p>
+          </div>
+        )}
      </div>
      </div>
    );
