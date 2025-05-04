45078ce3d7816c8b1aa82f7eb0d207dbf84560ad
myousefg <myousefg@gmail.com>
2025-05-04 17:49:06 +0700

    Backend: Hapus file Laravel dan date.js

--- uploads/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExcelController;
-
-Route::get('/excel-data', [ExcelController::class, 'getExcelData']);
-Route::delete('/delete-excel', [ExcelController::class, 'deleteExcelFile']);
