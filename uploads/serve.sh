9ecfdb0ea220d7b0751819fccbfc7fba2fb71a0c
Shua <jo.daniel.simanjuntak@gmail.com>
2025-05-06 01:46:57 +0700

    serve.sh: Hapus komentar

--- uploads/serve.sh
+++ uploads/serve.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-# Function to handle SIGINT (Ctrl+C)
 cleanup() {
-  echo "Interrupt received. Terminating all child processes..."
-  kill 0  # Sends SIGINT to all processes in the current process group
-  wait    # Wait for all child processes to exit
+  echo "Interrupt received."
+  kill 0
+  wait
   exit 1
 }
 
-# Trap SIGINT and call the cleanup function
 trap cleanup INT
 
-# Start Python scripts in the background
 python tableQuery.py &
 python date.py &
 streamlit run upload.py &
 
-# Wait for all background processes to complete
 wait
 
